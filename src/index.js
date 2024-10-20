e84f56d4523684b0b0e4bef34e131baa4a5d2fc1
Valentina <vjss05@gmail.com>
2024-10-20 16:17:36 +0200

    read resourcers for users and tasks

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-
+// Create a new User
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
@@ -21,6 +21,33 @@ app.post("/users", (req, res) => {
     });
 });
 
+// Get all Users
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(505).send(err);
+    });
+});
+
+// Get a single User by ID
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found." });
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// Create a new Task
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
@@ -30,6 +57,32 @@ app.post("/tasks", (req, res) => {
     })
     .catch((err) => {
       res.status(400).send(err);
+    });
+});
+
+// Get all Tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
+// Get a single Task by ID
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send({ message: "Task not found." });
+      }
+      res.send(task);
+    })
+    .catch((err) => {
+      res.status(505).send();
     });
 });
 
